8abf20ed2edb47883b84a1fa67d6a0470723cf7e
ZeenAE <zeenae@trollagent.dev>
2026-09-19 10:36:46 +0800

    v2.9.325: 输入框按微信标准调整(textInset top/bottom 4)

--- Sources/TrollMCP2/ChatView.swift
+++ Sources/TrollMCP2/ChatView.swift
@@ -1523,7 +1523,7 @@ struct ChatInputTextView: UIViewRepresentable {
         tv.font = .systemFont(ofSize: 16)
         tv.backgroundColor = .clear
         tv.isScrollEnabled = false
-        tv.textContainerInset = UIEdgeInsets(top: 6, left: 6, bottom: 6, right: 6)
+        tv.textContainerInset = UIEdgeInsets(top: 4, left: 8, bottom: 4, right: 8)
         tv.textContainer.lineFragmentPadding = 0
         tv.textContainer.widthTracksTextView = true
         tv.returnKeyType = .default
